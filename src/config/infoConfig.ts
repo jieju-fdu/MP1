4337e08e55955daf7f34876e6c772644359e8939
jujie <fiddle@jujiedeMacBook-Pro.local>
2025-03-16 21:52:23 +0800

    update something

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,12 +3,12 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'Jujie'
+export const headline = 'Student at Fudan University.'
 export const introduction =
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
+  "Hello! My name is Jujie. I'm from Jiangsu. I’ve been  Computer Science at SCLS since 2020."
 export const email = 'tong.hu@scls-sh.org'
-export const githubUsername = 'derekhut'
+export const githubUsername = 'Jujie'
 
 // about page
 export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
